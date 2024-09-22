d3cab3446db346c8b8befd7089d008e4a411ca73
MiiMGuel <miiguel065+github.com@google.com>
2024-09-22 21:05:34 +0700

    grey scale tested

--- test.c
+++ test.c
@@ -6,12 +6,20 @@
 #include "tga_image.h"
 
 int main(int argc, char* argv[]) {
-    tga_image_t* image = tga_image_read("in.tga");
+    tga_image_t* image = tga_image_read("images/in.tga");
     if (image == NULL) printf("[error] failed reading file!\n"); 
-    if (tga_image_write(image, "out.tga") != true) printf("[error] failed writing file\n");
+    if (tga_image_write(image, "images/out.tga") != true) printf("[error] failed writing file\n");
     if (tga_image_rle(image) != true) printf("[error] failed encoding file!\n");
     // if (tga_image_rld(image) != true) printf("[error] failed decoding file!\n");
-    if (tga_image_write(image, "outc.tga") != true) printf("[error] failed writing file\n");
+    if (tga_image_write(image, "images/outc.tga") != true) printf("[error] failed writing file\n");
+    tga_image_delete(image);
+
+    image = tga_image_read("images/gs_in.tga");
+    if (image == NULL) printf("[error] failed reading file!\n"); 
+    if (tga_image_write(image, "images/gs_out.tga") != true) printf("[error] failed writing file\n");
+    if (tga_image_rle(image) != true) printf("[error] failed encoding file!\n");
+    // if (tga_image_rld(image) != true) printf("[error] failed decoding file!\n");
+    if (tga_image_write(image, "images/gs_outc.tga") != true) printf("[error] failed writing file\n");
     tga_image_delete(image);
     return 0;
 }
